0a85b9fc9359835e3e907256f9ae3975d9748757
Victor234256 <charity.jv.jvo@gmail.com>
2024-06-11 20:36:37 +0100

    newly added

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,5 @@
-const { defineConfig } = require("@vue/cli-service");
-
-module.exports = defineConfig({
-  transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === "production" ? "/repo-finda/" : "/",
-});
+module.exports = {
+  transpileDependencies: [],
+  publicPath:
+    process.env.NODE_ENV === "production" ? "/repo-finda.github.io/" : "/",
+};
